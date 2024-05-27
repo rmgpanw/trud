9ef2e60c0ed10eb1d7f9c17964affc1bffced959
rmgpanw <rmgpanw@ucl.ac.uk>
2024-05-27 01:01:43 +0100

    add tests for `download_item()`

--- tests/testthat/test-download_item.R
+++ tests/testthat/test-download_item.R
@@ -6,3 +6,13 @@ test_that("Expected errors raised for invalid `release` arg with `download_item(
   expect_error(download_item(394, release = "invalid_release_arg"),
                "See available releases with `get_item_metadata\\(item = 394, latest_only = FALSE\\)`")
 })
+
+test_that("", {
+  skip_if_offline()
+  x <- download_item(394, directory = tempdir())
+
+  expect_true(file.exists(x))
+
+  expect_warning(download_item(394, directory = tempdir()), "already exists in directory")
+
+})
